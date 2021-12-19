27043e1695092b0cf773bec45ebf8b569bb0bf24
Greg V <greg@unrelenting.technology>
2021-12-19 22:30:53 +0300

    Track workspaces' identities and actually restore fullscreen apps to where they were fullscreened from

--- dynspaces.cpp
+++ dynspaces.cpp
@@ -1,6 +1,8 @@
 #define WAYFIRE_PLUGIN
 #define WLR_USE_UNSTABLE
 
+#include <vector>
+
 #include <wayfire/core.hpp>
 #include <wayfire/nonstd/wlroots-full.hpp>
 #include <wayfire/output.hpp>
@@ -32,8 +34,10 @@ static nonstd::observer_ptr<wf::grid::grid_animation_t> ensure_grid_view(wayfire
 }
 
 struct dynspaces_appspace : public wf::custom_data_t {
-	wf::point_t ws;
-	dynspaces_appspace(wf::point_t _ws) : ws(_ws) {}
+	uint64_t wsid_launched_from, wsid_ours;
+	wf::geometry_t relative_windowed_geom;
+	dynspaces_appspace(uint64_t _w1, uint64_t _w2, wf::geometry_t _g)
+	    : wsid_launched_from(_w1), wsid_ours(_w2), relative_windowed_geom(_g) {}
 };
 
 struct dynspaces_workspace_implementation_t : public wf::workspace_implementation_t {
@@ -44,64 +48,111 @@ struct dynspaces_workspace_implementation_t : public wf::workspace_implementatio
 };
 
 struct wayfire_dynspaces : public wf::plugin_interface_t {
-	wf::geometry_t add_workspace(wf::geometry_t geometry, wf::point_t delta) {
-		auto scr_size = output->get_screen_size();
-		geometry.x += delta.x * scr_size.width;
-		geometry.y += delta.y * scr_size.height;
-		return geometry;
+	inline wf::point_t ws_point(int num) {
+		// TODO: if (vertical) ..
+		return {num, 0};
 	}
 
-	void make_x_room_after(wf::point_t ws) {
+	inline int point_ws(wf::point_t wsp) {
+		// TODO: if (vertical) ..
+		return wsp.x;
+	}
+
+	inline int main_coord(wf::geometry_t geo) {
+		// TODO: if (vertical) ..
+		return geo.x;
+	}
+
+	inline int num_workspaces() {
 		auto grid_size = output->workspace->get_workspace_grid_size();
-		auto scr_size = output->get_screen_size();
-		for (size_t i = grid_size.width - 1; i > ws.x; i--) {
-			wf::point_t iws{static_cast<int>(i), ws.y};
-			auto views =
-			    output->workspace->get_views_on_workspace(iws, wf::LAYER_MINIMIZED | wf::WM_LAYERS);
-			for (auto v : views) {
-				if (output->workspace->get_view_main_workspace(v) == iws) {
-					auto box = v->get_wm_geometry();
-					v->move(box.x + scr_size.width, box.y);
-				}
-			}
-		}
-		// NOTE: doing this *after* the view moves to avoid get_view_main_workspace returning the newly
-		// created one
+		// TODO: if (vertical) ..
+		return grid_size.width;
+	}
+
+	inline int current_ws() { return point_ws(output->workspace->get_current_workspace()); }
+
+	inline void switch_to(int ws) { output->workspace->request_workspace(ws_point(ws)); }
+
+	void increase_grid() {
+		auto grid_size = output->workspace->get_workspace_grid_size();
+		// TODO: if (vertical) ..
 		grid_size.width++;
 		output->workspace->set_workspace_grid_size(grid_size);
 	}
 
-	void shrink_x_room_after(wf::point_t ws) {
+	void decrease_grid() {
 		auto grid_size = output->workspace->get_workspace_grid_size();
-		auto scr_size = output->get_screen_size();
-		for (size_t i = ws.x; i < grid_size.width; i++) {
-			wf::point_t iws{static_cast<int>(i), ws.y};
-			auto views =
-			    output->workspace->get_views_on_workspace(iws, wf::LAYER_MINIMIZED | wf::WM_LAYERS);
-			for (auto v : views) {
-				// XXX: WTF - all geometry is already shifted 1 workspace to the left here?!?!
-				wf::point_t iws2{static_cast<int>(i - 1), ws.y};
-				if (output->workspace->get_view_main_workspace(v) == iws2) {
-					auto box = v->get_wm_geometry();
-					v->move(box.x - scr_size.width, box.y);
-				}
-			}
-		}
+		// TODO: if (vertical) ..
 		grid_size.width--;
 		output->workspace->set_workspace_grid_size(grid_size);
+	}
+
+	// Wayfire workspaces are really just position offsets,
+	// we need our own identity layer:
+	uint64_t next_id = 0;
+	std::vector<uint64_t> wsids;
+
+	int ws_by_id(uint64_t wsid) {
+		return std::distance(wsids.begin(), std::find(wsids.begin(), wsids.end(), wsid));
+	}
+
+	// Takes relative geometry ("just window position without workspaces"),
+	// places it on a workspace (absolute number),
+	// but the result is relative to the current workspace (ready for set_geometry)
+	wf::geometry_t place_at_ws(wf::geometry_t geometry, int ws) {
+		auto scr_size = output->get_screen_size();
+		auto ws_delta = ws_point(ws) - output->workspace->get_current_workspace();
+		geometry.x += ws_delta.x * scr_size.width;
+		geometry.y += ws_delta.y * scr_size.height;
+		return geometry;
+	}
+
+	int add_ws_after(int ws) {
+		auto num = num_workspaces();
+		auto scr_size = output->get_screen_size();
+		wsids.push_back(UINT64_MAX);
+		for (int i = num - 1; i > ws; i--) wsids[i + 1] = wsids[i];
+		auto views = output->workspace->get_views_in_layer(wf::LAYER_MINIMIZED | wf::WM_LAYERS);
+		for (auto v : views) {
+			auto rel_geo = place_at_ws(v->get_wm_geometry(), -1 * (ws + 1));
+			if (main_coord(rel_geo) >= 0) {
+				auto box = v->get_wm_geometry();
+				v->move(box.x + scr_size.width, box.y);
+			}
+		}
+		// NOTE: doing this *after* the view moves to avoid get_view_main_workspace returning the newly
+		// created one
+		increase_grid();
+		wsids[ws + 1] = next_id++;
+		return ws + 1;
+	}
+
+	void destroy_ws(int ws) {
+		auto num = num_workspaces();
+		auto scr_size = output->get_screen_size();
+		for (int i = ws; i < num; i++) wsids[i] = wsids[i + 1];
+		auto views = output->workspace->get_views_in_layer(wf::LAYER_MINIMIZED | wf::WM_LAYERS);
+		for (auto v : views) {
+			auto rel_geo = place_at_ws(v->get_wm_geometry(), -1 * ws);
+			if (main_coord(rel_geo) >= 0) {
+				auto box = v->get_wm_geometry();
+				v->move(box.x - scr_size.width, box.y);
+			}
+		}
+		decrease_grid();
+		wsids.pop_back();
 	}
 
 	wf::signal_connection_t on_fullscreen = [=](wf::signal_data_t *ev) {
 		auto data = static_cast<wf::view_fullscreen_request_signal *>(ev);
 		if (data->state) {
-			// NOTE: not data->workspace, we *do* want to go next-of-*current*
-			auto ws = output->workspace->get_current_workspace();
-			make_x_room_after(ws);
-			ws.x++;
-			ensure_grid_view(data->view)
-			    ->adjust_target_geometry(add_workspace(data->desired_size, {1, 0}), -1);
-			output->workspace->request_workspace(ws);
-			data->view->store_data(std::make_unique<dynspaces_appspace>(ws));
+			auto wsid_launched_from = wsids[current_ws()];
+			auto rel_geom = data->view->get_wm_geometry();
+			auto ws = add_ws_after(current_ws());
+			ensure_grid_view(data->view)->adjust_target_geometry(place_at_ws(data->desired_size, ws), -1);
+			switch_to(ws);
+			data->view->store_data(
+			    std::make_unique<dynspaces_appspace>(wsid_launched_from, wsids[ws], rel_geom));
 		} else {
 			if (!data->view->has_data<dynspaces_appspace>()) {
 				// Possibly when the plugin was added dynamically after this view was created
@@ -109,12 +160,12 @@ struct wayfire_dynspaces : public wf::plugin_interface_t {
 				return;
 			}
 			auto appspace = data->view->get_data<dynspaces_appspace>();
-			// TODO: restore to the closest *non-app* space to the left
 			ensure_grid_view(data->view)
-			    ->adjust_target_geometry(add_workspace(data->desired_size, {-1, 0}), -1);
-			shrink_x_room_after(appspace->ws);
-			appspace->ws.x--;
-			output->workspace->request_workspace(appspace->ws);
+			    ->adjust_target_geometry(
+			        place_at_ws(appspace->relative_windowed_geom, ws_by_id(appspace->wsid_launched_from)),
+			        -1);
+			switch_to(ws_by_id(appspace->wsid_launched_from));
+			destroy_ws(ws_by_id(appspace->wsid_ours));
 			data->view->erase_data<dynspaces_appspace>();
 		}
 		data->carried_out = true;
@@ -126,7 +177,7 @@ struct wayfire_dynspaces : public wf::plugin_interface_t {
 		auto data = static_cast<wf::view_minimize_request_signal *>(ev);
 		if (data->view->has_data<dynspaces_appspace>() && data->state) {
 			auto appspace = data->view->get_data<dynspaces_appspace>();
-			data->view->fullscreen_request(output, false, appspace->ws);
+			data->view->fullscreen_request(output, false, ws_point(ws_by_id(appspace->wsid_ours)));
 			// This will emit the view-fullscreen-request signal we handle above
 		}
 	};
@@ -136,12 +187,16 @@ struct wayfire_dynspaces : public wf::plugin_interface_t {
 		if (data->view->has_data<dynspaces_appspace>()) {
 			// We're trying hard to prevent this but in case it happens let's clean up
 			auto appspace = data->view->get_data<dynspaces_appspace>();
-			shrink_x_room_after(appspace->ws);
+			destroy_ws(ws_by_id(appspace->wsid_ours));
 			data->view->erase_data<dynspaces_appspace>();
 		}
 	};
 
 	void init() override {
+		output->workspace->set_workspace_grid_size({2, 1});
+		wsids.reserve(16);
+		wsids.push_back(next_id++);
+		wsids.push_back(next_id++);
 		output->workspace->set_workspace_implementation(
 		    std::make_unique<dynspaces_workspace_implementation_t>(), true);
 		output->connect_signal("view-fullscreen-request", &on_fullscreen);
